c257f8f6b5c5392cb8d08d1a3bb33cb1503dd8ba
LivisonVinicius <livisongc@gmail.com>
2022-04-14 20:53:52 -0300

    Envio de mensagem implementado

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let nameUser;
+let destinatario="Todos";
+let mensagemDigitada;
+let tipoMensagem="message";
 function sideBar(){
     document.querySelector(".fundoSide").classList.add("active");
     document.querySelector(".sideBar").classList.add("active");
@@ -8,19 +11,21 @@ function hideBar(){
     document.querySelector(".sideBar").classList.remove("active");
 }
 function login(){
-    nameUser=document.querySelector(".entrada input").value;
-    const tryLogin=axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",{ name:`${nameUser}`})
-    tryLogin.then(function(response){
-        document.querySelector(".entrada").classList.add("active")
+    nameUser=document.querySelector(".entrada input");
+    const tryLogin=axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",{ name:`${nameUser.value}`})
+    tryLogin.then(function(){
+        document.querySelector(".entrada").classList.add("active");
+        document.querySelector("header").classList.add("active");
     })
-    tryLogin.catch(function(response){
+    tryLogin.catch(function(){
         alert("Usuário já se encontra online.")
     })
-    setInterval(conection,4000)
+    setInterval(conection,3000)
     setInterval(buscaMensagens,3000)
+    participantsOnline()
 }
 function conection(){
-    const mantemLogin=axios.post("https://mock-api.driven.com.br/api/v6/uol/status",{ name:`${nameUser}`})
+    const mantemLogin=axios.post("https://mock-api.driven.com.br/api/v6/uol/status",{ name:`${nameUser.value}`})
     mantemLogin.then(function(response){
         response.data
     })
@@ -35,25 +40,65 @@ function buscaMensagens(){
             switch(response.data[contador].type){
             case "status":
                 main.innerHTML+=`<div class="chatStatus">
-                <span class="time">(${response.data[contador].time})</span> ${response.data[contador].from} ${response.data[contador].text}
+                <p><span class="time">(${response.data[contador].time})</span> ${response.data[contador].from} ${response.data[contador].text}</p>
             </div>`
             break
             case "message":
                 main.innerHTML+=`<div class="chatMessage">
-                <span class="time">(${response.data[contador].time})</span> ${response.data[contador].from} para ${response.data[contador].to}: ${response.data[contador].text}
+                <p><span class="time">(${response.data[contador].time})</span> ${response.data[contador].from} para ${response.data[contador].to}: ${response.data[contador].text}</p>
             </div>`
             break
             case "private_message":
-                main.innerHTML+=`<div class="chatPrivate">
-                <span class="time">(${response.data[contador].time})</span> ${response.data[contador].from} reservadamente para ${response.data[contador].to}: ${response.data[contador].text}
+                if(response.data[contador].from==nameUser.value ||response.data[contador].to==nameUser.value){
+                    main.innerHTML+=`<div class="chatPrivate">
+                <p><span class="time">(${response.data[contador].time})</span> ${response.data[contador].from} reservadamente para ${response.data[contador].to}: ${response.data[contador].text}</p>
             </div>`
+                }          
             }
             contador++
         }
         let screenHeight = "" + window.innerHeight / 8;
         main.scrollIntoView(false);
         window.scrollBy(0, screenHeight);
-        
     })
-    
+}
+function participantsOnline(){
+    const promise=axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+    promise.then(function(response){
+        let contador=0
+        let users=document.querySelector(".users")
+        users.innerHTML=""
+        while(contador<response.data.length){
+            users.innerHTML+= `<div class="usuario" onclick=selecionar(this)>
+            <ion-icon name="people"></ion-icon> <p>${response.data[contador].name}</p> <img src="imagens/Vector.png" alt="selecionado"/>
+        </div>`
+            contador++
+        }
+        setTimeout(participantsOnline,10000)
+    })
+}
+function enviarMensagem(){
+    mensagemDigitada=document.querySelector("footer input")
+    const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",{
+        from:`${nameUser.value}`,
+        to:`${destinatario}`,
+        text:`${mensagemDigitada.value}`,
+        type:`${tipoMensagem}`
+    })
+    promise.then(function(){
+        buscaMensagens()
+        mensagemDigitada.value=""
+    })
+    promise.catch(function(){
+        alert("Usuário não está online")
+        window.location.reload()
+    })
+}
+function selecionar(opcao){
+    const opcaoSelecionada = document.querySelector(`.Selecionada`);
+    if (opcaoSelecionada !== null) {
+        opcaoSelecionada.classList.remove("Selecionada");
+    }
+    opcao.classList.add("Selecionada");
+    destinatario=opcao.querySelector("p").innerHTML
 }
