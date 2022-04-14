3ad45c7a321e873b1f5cc00c129f81c380b17df7
LivisonVinicius <livisongc@gmail.com>
2022-04-13 23:03:02 -0300

    Layout

---
+++ script.js
@@ -0,0 +1,8 @@
+function sideBar(){
+    document.querySelector(".fundoSide").classList.add("active");
+    document.querySelector(".sideBar").classList.add("active");
+}
+function hideBar(){
+    document.querySelector(".fundoSide").classList.remove("active");
+    document.querySelector(".sideBar").classList.remove("active");
+}
